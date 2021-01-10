eed179d4848ce826971706878357efbc86ae41f9
kamil.korczak <kamil.korczak@gmail.com>
2021-01-10 22:08:12 +0100

    Add volume endpoint

--- endpoints/webSocketEndpoints.js
+++ endpoints/webSocketEndpoints.js
@@ -78,6 +78,9 @@ module.exports.getWebSocketEndpoints = (wsServer) => {
                 case "checkState":
                     sendStatus(ws, playerState);
                     break;
+                case "volume":
+                    mplayer.volume(messageContent.parameter);
+                    break;
             }
         });
 
